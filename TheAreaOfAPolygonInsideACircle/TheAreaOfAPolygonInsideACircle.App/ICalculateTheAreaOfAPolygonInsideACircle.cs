604a3bcf2720c6006e1f4525484358ab845fb5f4
Sean Pietersen <seanpietersen7@gmail.com>
2022-04-12 19:37:58 +0200

    Feat: Added App

---
+++ TheAreaOfAPolygonInsideACircle/TheAreaOfAPolygonInsideACircle.App/ICalculateTheAreaOfAPolygonInsideACircle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheAreaOfAPolygonInsideACircle.App
+{
+    public interface ICalculateTheAreaOfAPolygonInsideACircle
+    {
+        double AreaOfPolygonInsideCircle(double circleRadius, int numberOfSides);
+    }
+}
--- TheAreaOfAPolygonInsideACircle/TheAreaOfAPolygonInsideACircle.Test/CalculateTheAreaOfAPolygonInsideACircleTest.cs
+++ TheAreaOfAPolygonInsideACircle/TheAreaOfAPolygonInsideACircle.Test/CalculateTheAreaOfAPolygonInsideACircleTest.cs
@@ -1,4 +1,4 @@
-using System;
+using TheAreaOfAPolygonInsideACircle.App;
 using Xunit;
 
 namespace TheAreaOfAPolygonInsideACircle.Test
@@ -6,9 +6,9 @@ namespace TheAreaOfAPolygonInsideACircle.Test
     public class CalculateTheAreaOfAPolygonInsideACircleTest
     {
         [Theory]
-        [InlineData(3, 3, 11.69)]
+        [InlineData(3, 3, 11.691)]
         [InlineData(2, 4, 8)]
-        [InlineData(2.5,5, 14.86)]
+        [InlineData(2.5, 5, 14.86)]
 
         public void CalculatorTest1_Successful(double circleRadius, int numberOfSides, double expected)
         {
@@ -16,11 +16,10 @@ namespace TheAreaOfAPolygonInsideACircle.Test
             ICalculateTheAreaOfAPolygonInsideACircle calculateArea = new CalculateTheAreaOfAPolygonInsideACircle();
 
             //Act
-            var actual = calculateArea.Calculator(circleRadius, numberOfSides);
+            var actual = calculateArea.AreaOfPolygonInsideCircle(circleRadius, numberOfSides);
 
             //Assert
             Assert.Equal(expected, actual);
-
         }
     }
 }
